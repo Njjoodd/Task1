238a8fd0058b9552caf34fc9934143b2c54288e1
Njjoodd <nejood03@gmail.com>
2025-05-20 14:15:42 +0300

    states task 1-3 are done, working on 4

--- app/src/main/java/com/first/task1/MainActivity.kt
+++ app/src/main/java/com/first/task1/MainActivity.kt
@@ -4,19 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.NotificationCompat.Style
 import com.first.task1.ui.theme.Task1Theme
 
 class MainActivity : ComponentActivity() {
@@ -34,67 +36,99 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Game(modifier: Modifier = Modifier) {
-Column(
-    modifier = modifier
-        .fillMaxSize()
-        .padding(24.dp),
-    verticalArrangement = Arrangement.SpaceBetween
-){
-    Text(
-            text = "Android is an operating system.",
+fun Game(modifier: Modifier = Modifier)
+{
+   val questions = listOf(
+       "Android is an operating system." to true,
+       "Kotlin is developed by Microsoft" to false,
+       "XML is used for UI in Android" to true)
+
+    var questionIndex by remember { mutableStateOf(0) }
+    var isCorrect by remember { mutableStateOf<Boolean?>(null) }
+    val (question, correctAnswer) = questions[questionIndex]
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = question,
             style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.padding(bottom = 16.dp),
-
+            modifier = Modifier.padding(bottom = 16.dp)
         )
-Row(
-    modifier = Modifier.fillMaxWidth(),
 
-) {
-    Button(
-        onClick = { /* TODO: Handle True */ },
-        modifier = Modifier.weight(1f)
-    ) {
-        Text("True")
-    }
-                Spacer(modifier = Modifier.width(16.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(
+                onClick = { isCorrect = true },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = stringResource(R.string.text_true))
+            }
 
-                Button(
-                onClick = { /* TODO: Handle False */ },
-        modifier = Modifier.weight(1f)
-    ) {
-        Text("False")
-    }
-}
-        Result(text = "Correct Answer", modifier = Modifier.align(Alignment.CenterHorizontally))
+            Spacer(modifier = Modifier.width(16.dp))
 
-    Button(
-        onClick = {/* TODO: Go to next question*/},
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 16.dp, bottom = 8.dp)
+            Button(
+                onClick = { isCorrect = false },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = stringResource(R.string.text_false))
+            }
+        }
+
+        if (isCorrect != null) {
+            Result(
+                isCorrect = isCorrect!!,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
+
+        Button(
+            onClick = {
+                questionIndex = (questionIndex + 1) % questions.size
+                isCorrect = null
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 8.dp)
         ) {
-        Text("Next Question")
-    }
+            Text(stringResource(R.string.next_question))
+        }
     }
 }
 
 @Composable
-fun Result(text: String, modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier
-            .size(180.dp)
-            .background(Color.Green, shape = CircleShape),
-        contentAlignment = Alignment.Center
+fun Result(isCorrect: Boolean, modifier: Modifier = Modifier) {
+    val text = stringResource(if (isCorrect) R.string.correct_answer else R.string.wrong_answer)
+    val imageRes = if (isCorrect) R.drawable.correct_answer else R.drawable.wrong_answer
+    val bgColor = if (isCorrect) Color.Green else Color.Red
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = text,
-            color = Color.Black,
-            fontSize = 18.sp
+        Image(
+            painter = painterResource(id = imageRes),
+            contentDescription = text,
+            modifier = Modifier
+                .size(100.dp)
+                .padding(bottom = 8.dp)
         )
+        Box(
+            modifier = Modifier
+                .size(180.dp)
+                .background(bgColor, shape = CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text,
+                color = Color.Black,
+                fontSize = 18.sp
+            )
+        }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
